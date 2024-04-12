22336a6d6a315cd2f319b15865c60ad2fb09620e
rhythm2107 <kamil.krzywda.21@gmail.com>
2024-04-12 16:23:32 +0200

    Attempt to use one add function for two fields

--- setup.mjs
+++ setup.mjs
@@ -1,20 +1,20 @@
 export function setup(ctx) {
     ctx.settings.section('General').add(
     {
-      type: 'number',
-      name: 'xp-multiplier',
-      label: 'XP Multiplier',
-      hint: 'Multiply all XP gains by this amount',
-      default: 1
+        type: 'number',
+        name: 'xp-multiplier',
+        label: 'XP Multiplier',
+        hint: 'Multiply all XP gains by this amount',
+        default: 1
     },
     {
         type: 'text',
         name: 'Babuś',
-        label: 'Babuś to jest zwierze, babuś babuś babuś',
-        hint: 'Babuś babuś babuś',
-        default: 'Babuśiusususuś'
+        label: 'Babuś babus babus',
+        hint: 'Babuuuuś babus buus bus bus to jest zwierze',
+        default: 'Babuś'
     }
-);
+  );
   
     ctx.patch(Skill, 'addXP').before(function(amount, masteryAction) {
       const xpMultiplier = ctx.settings.section('General').get('xp-multiplier');
